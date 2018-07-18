6ad7b4dfb889695134b6e7a332959512e0d5a650
minsub <welcltm@gmail.com>
2018-07-18 23:55:11 +0900

    update

---
+++ src/main/java/studentRequest/StudentRequestApplication.java
@@ -0,0 +1,12 @@
+package studentRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRequestApplication.class, args);
+	}
+}
