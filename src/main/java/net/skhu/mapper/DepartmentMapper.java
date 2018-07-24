395a5b4a8069fde820895bb88587e35c129e76d6
minsub <welcltm@gmail.com>
2018-07-25 00:35:25 +0900

    update

---
+++ src/main/java/net/skhu/mapper/DepartmentMapper.java
@@ -0,0 +1,16 @@
+package net.skhu.mapper;
+
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import net.student.dto.Department;
+
+@Mapper
+public interface DepartmentMapper {
+
+	List<Department> findAll();
+
+
+}
